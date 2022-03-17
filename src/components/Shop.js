30d7ab60360f7a90dd0dcf706dfe985fd757c09c
maciej1kus <maciej1kus@gmail.com>
2022-03-17 15:25:04 +0100

    shop component update v2

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,23 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Navbar } from "./Navbar";
 import { Products } from "./Products";
-import { useHistory } from "react-router-dom";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../config/Config";
 import { HeaderLandingPage } from "./HeaderLandingPage";
 
 
 export const Shop = ({ user }) => {
-
-    const history = useHistory();
-
-    useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
-            if (!user) {
-                history.push('/login');
-            }
-        })
-    })
 
     return (
         <div className="wrapper">
