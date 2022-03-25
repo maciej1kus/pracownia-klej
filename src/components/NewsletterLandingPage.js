45914708a0c44994757ebc706a54ae39ba98e18d
maciej1kus <maciej1kus@gmail.com>
2022-03-25 17:29:05 +0100

    newsletter section - sending data to Firestore Database

--- src/components/NewsletterLandingPage.js
+++ src/components/NewsletterLandingPage.js
@@ -4,16 +4,32 @@ import { collection, addDoc } from "firebase/firestore";
 
 export const NewsletterLandingPage = () => {
 
+    const [email, setEmail] = useState('');
+    const [error, setError] = useState('');
+
+    //Zapisywanie się do newslettera
+    const signup = (e) => {
+        e.preventDefault();
+
+        addDoc(collection(db, "NewsletterList"), {
+            Email: email
+        }).then(() => {
+            setEmail('');
+        }).catch(err => setError(err.message));
+    }
+
     return (
         <section className="newsletter">
             <div className="newsletter__container container">
-                <form>
+                <form onSubmit={signup}>
                     <h3>Zapisz się do newslettera!</h3>
-                    <p>Bądź na bieżąco z nowinkami ze świata ceramiki oraz otrzymuj w pierwszej kolejności informację o promocjach.</p>
+                    <p>Bądź na bieżąco z nowinkami ze świata ceramiki oraz otrzymuj w pierwszej kolejności informacje o promocjach.</p>
                     <div className="email-box">
                         <i className="fa fa-envelope" />
-                        <input className="tbox" type="email" placeholder="Wpisz swój adres e-mail"/>
+                        <input className="tbox" type="email" placeholder="Wpisz swój adres e-mail"
+                               onChange={(e) => setEmail(e.target.value)} value={email} />
                         <button className="newsletter-btn" type="submit" name="button">Zapisuję się</button>
+                        {error && <span>{error}</span>}
                     </div>
                 </form>
             </div>
