fc1835847dbd74e417cf894ba9fb32e6b0878f60
maciej1kus <maciej1kus@gmail.com>
2022-03-26 17:29:18 +0100

    added contact section including styling

--- src/components/ContactLandingPage.js
+++ src/components/ContactLandingPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
+
 export const ContactLandingPage = () => {
     return (
         <section className="contact" id="contact">
@@ -8,8 +9,23 @@ export const ContactLandingPage = () => {
                 <div className="contact__column">
                     <h4>Dla klienta</h4>
                     <ul>
-                        <li></li>
+                        <li><Link to="faq" className="col-navlink">FAQ</Link></li>
+                        <li><Link to="delivery-and-returns" className="col-navlink">Dostawa i zwroty</Link></li>
+                        <li><Link to="privacy-policy" className="col-navlink">Polityka prywatności</Link></li>
+                        <li><Link to="terms-and-conditions" className="col-navlink">Regulamin</Link></li>
                     </ul>
+                </div>
+                <div className="contact__column">
+                    <h4>Kontakt</h4>
+                    <a href="mailto:pracowniaklej@gmail.com" className="col-navlink">
+                        <i className="fa fa-envelope email-icon"/>
+                        pracowniaklej@gmail.com
+                    </a>
+                    <p>ul. Poznańska 1 <br/>60-600 Poznań</p>
+                    <a href="tel:+48123456789" className="col-navlink">
+                        <i className="fa fa-phone phone-icon"/>
+                        + 48 123 456 789
+                    </a>
                 </div>
             </div>
         </section>
